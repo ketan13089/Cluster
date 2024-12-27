2467509b78367d2811b8b7c6e551a7abb21eec26
Ketan Ghumare <ketanghumare999@gmail.com>
2024-12-27 14:46:16 +0530

    chat list prototype

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -1,8 +1,97 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './chatList.css'
 
 export default function ChatList() {
+
+    const [addMode,setAddMode] = useState(false);
+
+
   return (
-    <div className='chatList'>ChatList</div>
+    <div className='chatList'>
+        <div className="search">
+            <div className="searchBar">
+                <img src="./search.png" alt="" />
+                <input type="text" placeholder='Seach' />
+            </div>
+            <img src={addMode ? "./minus.png":"./plus.png"} 
+            className='add' 
+            alt="" 
+            onClick={()=> setAddMode((prev) => !prev)} 
+            />
+        </div>
+
+        <div className="item">
+            <img src="./avatar.png" alt="" />
+            <div className="texts">
+                <span>Jane doe</span>
+                <p>Hello</p>
+            </div>
+        </div>
+
+        <div className="item">
+            <img src="./avatar.png" alt="" />
+            <div className="texts">
+                <span>Jane doe</span>
+                <p>Hello</p>
+            </div>
+        </div>
+
+        <div className="item">
+            <img src="./avatar.png" alt="" />
+            <div className="texts">
+                <span>Jane doe</span>
+                <p>Hello</p>
+            </div>
+        </div>
+
+        <div className="item">
+            <img src="./avatar.png" alt="" />
+            <div className="texts">
+                <span>Jane doe</span>
+                <p>Hello</p>
+            </div>
+        </div>
+
+        <div className="item">
+            <img src="./avatar.png" alt="" />
+            <div className="texts">
+                <span>Jane doe</span>
+                <p>Hello</p>
+            </div>
+        </div>
+
+        <div className="item">
+            <img src="./avatar.png" alt="" />
+            <div className="texts">
+                <span>Jane doe</span>
+                <p>Hello</p>
+            </div>
+        </div>
+
+        <div className="item">
+            <img src="./avatar.png" alt="" />
+            <div className="texts">
+                <span>Jane doe</span>
+                <p>Hello</p>
+            </div>
+        </div>
+
+        <div className="item">
+            <img src="./avatar.png" alt="" />
+            <div className="texts">
+                <span>Jane doe</span>
+                <p>Hello</p>
+            </div>
+        </div>
+
+        <div className="item">
+            <img src="./avatar.png" alt="" />
+            <div className="texts">
+                <span>Jane doe</span>
+                <p>Hello</p>
+            </div>
+        </div>
+
+    </div>
   )
 }
